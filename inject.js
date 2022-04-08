d179fce27a533fa51b5203c89e913197f7b6da65
Thiago Henrique Hupner <thihup@gmail.com>
2022-04-08 08:04:10 -0300

    Allow to reinject script if the history tab was not loaded
    Fixes #9

--- inject.js
+++ inject.js
@@ -1,18 +1,20 @@
 (function () {
 
-  if (!window.monaco) {
-    var monacoEditor = document.createElement('script');
-    monacoEditor.src = "https://cdn.jsdelivr.net/gh/vanillawc/wc-monaco-editor@1/index.js";
-    monacoEditor.type = "module";
-    monacoEditor.onload = (e) => {
+  const injectChangeNodeScript = () => {
 
-      var script = document.createElement('script');
-      script.type = "module";
-      script.src = chrome.runtime.getURL('changeNode.mjs');
-      document.body.appendChild(script);
-    }
-    document.body.appendChild(monacoEditor);
+    const script = document.createElement('script');
+    script.type = "module";
+    script.src = chrome.runtime.getURL('changeNode.mjs');
+    document.body.appendChild(script);
+  };
+
+  if (window.document.getElementsByClassName("changehistory").length == 0 || window.monaco) {
     return
   }
+  const monacoEditor = document.createElement('script');
+  monacoEditor.src = "https://cdn.jsdelivr.net/gh/vanillawc/wc-monaco-editor@1/index.js";
+  monacoEditor.type = "module";
+  monacoEditor.onload = injectChangeNodeScript;
+  document.body.appendChild(monacoEditor);
 
-})();
+}) ();
